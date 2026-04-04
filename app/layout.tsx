4f53b8d1424f3568115ccd24382d41cf95e199fc
Michalexos <michalburan77@gmail.com>
2026-04-04 19:02:05 +0200

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
+import { Metadata } from "next";
 import "./globals.css";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { CartProvider } from "./context/CartContext";
 import { CartModalWrapper } from "./components/CartModalWrapper";
+
+export const metadata: Metadata = {
+  title: "CheapPass.eu | Herné Passy",
+  description: "Najlacnejšie herné passy pre Clash of Clans, Clash Royale a ďalšie hry.",
+  // Nastavenie ikonky webu
+  icons: {
+    icon: "/logo.png", // Tento súbor musí byť v priečinku public/logo.png
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
