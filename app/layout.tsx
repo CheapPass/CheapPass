7ad0131b7a6cb9cf5a4efeeacd700c7b17e3fd92
Michalexos <michalburan77@gmail.com>
2026-04-04 19:04:29 +0200

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,19 @@ import { Footer } from "./components/Footer";
 import { CartProvider } from "./context/CartContext";
 import { CartModalWrapper } from "./components/CartModalWrapper";
 
+// Definícia metadát pre celú aplikáciu vrátane ikonky s pozadím
 export const metadata: Metadata = {
   title: "CheapPass.eu | Herné Passy",
   description: "Najlacnejšie herné passy pre Clash of Clans, Clash Royale a ďalšie hry.",
-  // Nastavenie ikonky webu
+  // Nastavenie ikonky webu s gradientným pozadím
   icons: {
-    icon: "/logo.png", // Tento súbor musí byť v priečinku public/logo.png
+    icon: [
+      {
+        url: "/logo.png", // Cesta k obrázku image_a1c1d4.png, ktorý si nahráš do public priečinka
+        // Pre istotu môžeme definovať, že toto je aj favicon a apple-touch-icon
+        href: "/logo.png",
+      }
+    ],
     shortcut: "/logo.png",
     apple: "/logo.png",
   },
